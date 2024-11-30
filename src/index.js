4099a19f8039c672da519c4a3f887a1e626da200
Taha syeed <85818873+tahasyeed@users.noreply.github.com>
2024-11-30 16:22:03 +0530

    VIDEO AND USER MODEL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 
 import dotenv from "dotenv"
-
+import express from "express";
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -10,7 +10,7 @@ dotenv.config({
 })
 
 
-
+const app = express();
 
 
 connectDB()
