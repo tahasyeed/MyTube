0197a84ac83b1fc3261ce71d8afa9025e7195022
Taha syeed <85818873+tahasyeed@users.noreply.github.com>
2024-12-04 14:21:57 +0530

    multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,71 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+     const response =  await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+
+        //file has been uploaded successfully
+        console.log("file uploded on cloudinary succesfully", response.url);
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally savesd temp file as the operation got failed
+        return null;
+    }
+}
+
+export  {uploadOnCloudinary}
+
+
+
+
+// (async function() {
+
+//     // Configuration
+    
+
+// cloudinary.config({ 
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key: process.env.CLOUDINARY_API_KEY, 
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+    
+//     // Upload an image
+//      const uploadResult = await cloudinary.uploader
+//        .upload(
+//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                public_id: 'shoes',
+//            }
+//        )
+//        .catch((error) => {
+//            console.log(error);
+//        });
+    
+//     console.log(uploadResult);
+    
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+    
+//     console.log(optimizeUrl);
+    
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+    
+//     console.log(autoCropUrl);    
+// })();
