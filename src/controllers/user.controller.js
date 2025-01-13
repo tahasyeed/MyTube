80b692686bccbf88ddf13a38cd5144cfb07111d0
Taha syeed <85818873+tahasyeed@users.noreply.github.com>
2025-01-13 16:05:49 +0530

    Add user controller functions for changing password, getting current user, updating account details, and updating user avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,6 +232,68 @@ try {
     throw new ApiError(401, error?.message || "Invalid refresh token")
 }
 })
-export {registerUser, loginUser, logOutUser, refreshAccessToken}
+
+
+
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword, } = req.body
+
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+
+    }
+    
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+    
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "password changed succesfully "))
+
+})
+
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullname, email } = req.body;
+
+    if (!fullname || !email) {
+        throw new ApiError(400, "all fields are required")
+    }
+
+   const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname,
+                email: email
+            }
+        },
+        { new: true}
+    ).select("-password")
+
+    return res
+        .status
+    .json(new ApiResponse(200, user, "Accound details updated Successfully"))
+
+
+
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+})
+
+export {registerUser, loginUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar}
 
 
